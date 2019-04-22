a005fbee75ca460b49f074a9dd5a0713ddc711fe
Dionidgy <32619305+Dionidgy@users.noreply.github.com>
2019-04-22 21:08:54 +0300

    Add files via upload

---
+++ url_download.py
@@ -0,0 +1,7 @@
+import urllib.request
+import shutil
+url = input()
+file_name = url.split('/')[-1]
+# Download the file from `url` and save it locally under `file_name`:
+with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
